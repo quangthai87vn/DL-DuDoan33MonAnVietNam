ef5904d928b42ddb130be121fc918427422b8cfe
quangthai87@gmail.com <quangthai87@gmail.com>
2025-11-25 08:18:58 +0700

    ok

---
+++ YOLOv11-Object-Detector-iOS-CoreML-withNMS/YOLOv11-Object-Detector-iOS-CoreML/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  YOLOv11-Object-Detector-iOS-CoreML
+//
+//  Created by Surendra Maran on 01/09/25.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let modelName = "food_yolov11_best"//"yolo11s"
+}
